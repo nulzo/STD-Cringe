f751c494b0a33aa43f0d940c713bdd97633c9769
nulzo <nolanpgregory@gmail.com>
2024-01-06 17:48:18 -0600

    [update] - linux

---
+++ cmake-build/CMakeFiles/cringe.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/bruh/Desktop/STD-Cringe/src/commands/api/ChatCommand.cpp" "CMakeFiles/cringe.dir/src/commands/api/ChatCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/api/ChatCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/api/ImageCommand.cpp" "CMakeFiles/cringe.dir/src/commands/api/ImageCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/api/ImageCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/api/RedditCommand.cpp" "CMakeFiles/cringe.dir/src/commands/api/RedditCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/api/RedditCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/api/UserCommand.cpp" "CMakeFiles/cringe.dir/src/commands/api/UserCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/api/UserCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/challenge/ChallengeCommand.cpp" "CMakeFiles/cringe.dir/src/commands/challenge/ChallengeCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/challenge/ChallengeCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/misc/ConfessionCommand.cpp" "CMakeFiles/cringe.dir/src/commands/misc/ConfessionCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/misc/ConfessionCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/misc/EthanCommand.cpp" "CMakeFiles/cringe.dir/src/commands/misc/EthanCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/misc/EthanCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/misc/GetMessagesCommand.cpp" "CMakeFiles/cringe.dir/src/commands/misc/GetMessagesCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/misc/GetMessagesCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/misc/InfoCommand.cpp" "CMakeFiles/cringe.dir/src/commands/misc/InfoCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/misc/InfoCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/voice/JoinCommand.cpp" "CMakeFiles/cringe.dir/src/commands/voice/JoinCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/voice/JoinCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/voice/PlayCommand.cpp" "CMakeFiles/cringe.dir/src/commands/voice/PlayCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/voice/PlayCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/voice/QueueCommand.cpp" "CMakeFiles/cringe.dir/src/commands/voice/QueueCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/voice/QueueCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/commands/voice/SkipCommand.cpp" "CMakeFiles/cringe.dir/src/commands/voice/SkipCommand.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/commands/voice/SkipCommand.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/listeners/message_listener.cpp" "CMakeFiles/cringe.dir/src/listeners/message_listener.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/listeners/message_listener.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/main.cpp" "CMakeFiles/cringe.dir/src/main.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/main.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/utils/CringeEmbed.cpp" "CMakeFiles/cringe.dir/src/utils/CringeEmbed.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/utils/CringeEmbed.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/utils/audio/CringeAudio.cpp" "CMakeFiles/cringe.dir/src/utils/audio/CringeAudio.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/utils/audio/CringeAudio.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/utils/audio/CringeQueue.cpp" "CMakeFiles/cringe.dir/src/utils/audio/CringeQueue.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/utils/audio/CringeQueue.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/utils/audio/CringeSong.cpp" "CMakeFiles/cringe.dir/src/utils/audio/CringeSong.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/utils/audio/CringeSong.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/utils/logger.cpp" "CMakeFiles/cringe.dir/src/utils/logger.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/utils/logger.cpp.o.d"
+  "/home/bruh/Desktop/STD-Cringe/src/utils/util.cpp" "CMakeFiles/cringe.dir/src/utils/util.cpp.o" "gcc" "CMakeFiles/cringe.dir/src/utils/util.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build/CMakeFiles/cringe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cringe.
+# This may be replaced when dependencies are built.
