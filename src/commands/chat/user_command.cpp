8a9303fdcd1f9cf6a4b7c541e96f9c8bd2fa8e3f
nulzo <nolanpgregory@gmail.com>
2024-05-13 21:47:23 -0500

    [fix] - rework user command and embed

--- src/commands/chat/user_command.cpp
+++ src/commands/chat/user_command.cpp
@@ -39,41 +39,20 @@ void user_command(CringeBot &cringe, const dpp::slashcommand_t &event) {
     dpp::command_interaction cmd_data = event.command.get_command_interaction();
     dpp::snowflake user = cmd_data.get_value<dpp::snowflake>(0);
     dpp::user_identified user_t = cringe.cluster.user_get_sync(user);
-    dpp::guild_member guild_user_t =
-			cringe.cluster.guild_get_member_sync(event.command.get_guild().id, user);
+    dpp::guild_member guild_user_t = cringe.cluster.guild_get_member_sync(event.command.get_guild().id, user);
     std::string user_username = user_t.format_username();
     std::string user_avatar = user_t.get_avatar_url();
-    std::string user_created = fmt::format(
-        "<t:{}:D>", std::to_string((int)user_t.get_creation_time()));
-    std::string user_is_bot = (user_t.is_bot())
-                                  ? dpp::unicode_emoji::white_check_mark
-                                  : dpp::unicode_emoji::no_entry_sign;
-    std::string user_has_nitro =
-        (user_t.has_nitro_classic() || user_t.has_nitro_full() ||
-         user_t.has_nitro_basic())
-            ? dpp::unicode_emoji::white_check_mark
-            : dpp::unicode_emoji::no_entry_sign;
-    std::string user_joined_server =
-        fmt::format("<t:{}:D>", std::to_string((int)guild_user_t.joined_at));
-    std::string user_premium =
-        ((int)guild_user_t.premium_since > 0)
-            ? fmt::format("<t:{}:D>",
-                          std::to_string((int)guild_user_t.premium_since))
-            : dpp::unicode_emoji::no_entry_sign;
+    std::string user_created = fmt::format("<t:{}:D>", std::to_string((int)user_t.get_creation_time()));
+    std::string user_joined_server = fmt::format("<t:{}:D>", std::to_string((int)guild_user_t.joined_at));
     std::string user_mention = user_t.get_mention();
     std::string title = "Who is @" + user_username;
-    const int has_nitro = user_t.has_nitro_full() || user_t.has_nitro_basic() ||
-                          user_t.has_nitro_classic();
+	prompt = "You are writing a short description of a user. You must write what you think about the user. Be creative, lewd, and funny. The information about the user is given below.";
     const int has_boost = guild_user_t.premium_since > 0 ? 1 : 0;
     if (user_t.global_name == "nulzo") {
-        prompt =
-            "Make up really good things about a user named nulzo! They are so "
-            "awesome!";
+        prompt +=
+            "Make up really good things about a user named nulzo! They are so awesome!";
     } else {
-        prompt = fmt::format("Write a short description about some shit about a loser who goes by the name {}.", user_username);
-    }
-    if (has_nitro == 0) {
-        prompt += " Mention that the person doesn't have Discord Nitro. They are VERY lame for this!";
+        prompt += fmt::format("Write a short description about some shit about a loser who goes by the name {}.", user_username);
     }
     if (has_boost == 0) {
         prompt += " Include that this user is a stupid idiot who does not boost the Discord Server.";
